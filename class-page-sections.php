52c8be8e2e069b59fe4b109e093068a9ccf544f1
Benedicte Raae <raae@bgraphic.no>
2016-10-03 15:07:40 +0200

    Changed page sections to tax blocks

--- class-page-sections.php
+++ class-page-sections.php
@@ -5,7 +5,6 @@ namespace LSB\Boksok\Core;
 class PageSections {
 
 	var $taxonomies = array(
-		'lsb_tax_lsb_cat' => 'Hovedkategori',
 		'lsb_tax_topic' => 'Emne',
 		'lsb_tax_author' => 'Forfatter',
 		'lsb_tax_list' => 'Liste',
@@ -17,12 +16,12 @@ class PageSections {
 	);
 
 	public function __construct() {
-		add_action( 'acf/init', array( &$this, 'register_field_group_page_section_group' ) );
+		add_action( 'acf/init', array( &$this, 'register_taxonomy_field_group' ) );
 	}
 
-	public function section_tax_layouts( $section_type, $is_multi_select ) {
+	public function taxonomy_options ( $block_type, $is_multi_select ) {
 
-		$section_navigation_layouts = array();
+		$taxonomy_options = array();
 
 		$field_type = 'select';
 		if( $is_multi_select ) {
@@ -30,17 +29,17 @@ class PageSections {
 		}
 
 		foreach ($this->taxonomies as $key => $value) {
-			$section_navigation_layouts[] = array (
-				'key' => 'lsb_acf_page_section_'.$section_type.'_taxonomy_'.$key,
+			$taxonomy_options[] = array (
+				'key' => 'lsb_acf_block_'.$block_type.'_taxonomy_'.$key,
 				'label' => $value,
-				'name' => 'lsb_page_section_'.$section_type.'_taxonomy_'.$key,
+				'name' => 'lsb_block_'.$block_type.'_taxonomy_'.$key,
 				'display' => 'row',
 				'max' => 1,
 				'sub_fields' => array(
 					array(
-						'key' => 'lsb_acf_page_section_'.$section_type.'_taxonomy_'.$key.'_terms',
+						'key' => 'lsb_acf_block_'.$block_type.'_taxonomy_'.$key.'_terms',
 						'label' => '',
-						'name' => 'lsb_page_section_'.$section_type.'_taxonomy_'.$key.'_terms',
+						'name' => 'lsb_block_'.$block_type.'_taxonomy_'.$key.'_terms',
 						'type' => 'taxonomy',
 						'taxonomy' => $key,
 						'field_type' => $field_type,
@@ -52,8 +51,152 @@ class PageSections {
 			);
 		}
 
-		return $section_navigation_layouts;
+		return $taxonomy_options;
 	}
+
+	public function register_taxonomy_field_group() {
+
+		if( function_exists('acf_add_local_field_group') ) {
+
+			acf_add_local_field_group(array (
+				'key' => 'lsb_acf_taxonomy_blocks_group',
+				'title' => 'Taxonomy sections',
+				'fields' => array (
+					array (
+						'key' => 'lsb_acf_blocks',
+						'label' => 'Blokker',
+						'name' => 'lsb_blocks',
+						'type' => 'flexible_content',
+						'instructions' => '',
+						'required' => 0,
+						'conditional_logic' => 0,
+						'wrapper' => array (
+							'width' => '',
+							'class' => '',
+							'id' => '',
+						),
+						'button_label' => 'Legg til blokk',
+						'min' => '',
+						'max' => 10,
+						'layouts' => array (
+							array (
+								'key' => 'lsb_acf_tax_block_books',
+								'name' => 'lsb_tax_block_books',
+								'label' => 'Bøker',
+								'display' => 'block',
+								'sub_fields' => array (
+									array (
+										'key' => 'lsb_acf_tax_block_books_title',
+										'label' => 'Tittel',
+										'name' => 'lsb_tax_block_books_title',
+										'type' => 'text',
+										'instructions' => '',
+										'required' => 0,
+										'conditional_logic' => 0,
+										'wrapper' => array (
+											'width' => '',
+											'class' => '',
+											'id' => '',
+										),
+										'default_value' => '',
+										'placeholder' => '',
+										'prepend' => '',
+										'append' => '',
+										'maxlength' => '',
+									),
+									array (
+										'key' => 'lsb_acf_tax_block_books_filters',
+										'label' => 'Kriterie',
+										'name' => 'lsb_tax_block_books_filters',
+										'type' => 'flexible_content',
+										'instructions' => '',
+										'required' => 0,
+										'conditional_logic' => 0,
+										'wrapper' => array (
+											'width' => '',
+											'class' => '',
+											'id' => '',
+										),
+										'button_label' => 'Legg til kriterie',
+										'min' => '',
+										'max' => '',
+										'layouts' => $this->taxonomy_options('books', false)
+									),
+								),
+								'min' => '',
+								'max' => '',
+							),
+							array (
+								'key' => 'lsb_acf_tax_block_buttons',
+								'name' => 'lsb_tax_block_buttons',
+								'label' => 'Knapper',
+								'display' => 'block',
+								'sub_fields' => array (
+									array (
+										'key' => 'lsb_acf_tax_block_buttons_title',
+										'label' => 'Tittel',
+										'name' => 'lsb_tax_block_buttons_title',
+										'type' => 'text',
+										'instructions' => '',
+										'required' => 0,
+										'conditional_logic' => 0,
+										'wrapper' => array (
+											'width' => '',
+											'class' => '',
+											'id' => '',
+										),
+										'default_value' => '',
+										'placeholder' => '',
+										'prepend' => '',
+										'append' => '',
+										'maxlength' => '',
+									),
+									array (
+										'key' => 'lsb_acf_tax_block_buttons_filters',
+										'label' => 'Knapper',
+										'name' => 'lsb_tax_block_buttons_filters',
+										'type' => 'flexible_content',
+										'instructions' => '',
+										'required' => 0,
+										'conditional_logic' => 0,
+										'wrapper' => array (
+											'width' => '',
+											'class' => '',
+											'id' => '',
+										),
+										'button_label' => 'Velg knapp(er) fra',
+										'min' => '',
+										'max' => '',
+										'layouts' => $this->taxonomy_options('buttons', true)
+									),
+								),
+								'min' => '',
+								'max' => '',
+							),
+						),
+					),
+				),
+				'location' => array (
+					array (
+						array (
+							'param' => 'taxonomy',
+							'operator' => '==',
+							'value' => 'lsb_tax_lsb_cat',
+						),
+					),
+				),
+				'menu_order' => 0,
+				'position' => 'normal',
+				'style' => 'default',
+				'label_placement' => 'top',
+				'instruction_placement' => 'label',
+				'hide_on_screen' => '',
+				'active' => 1,
+				'description' => '',
+			));
+		}
+	}
+
 
 	public function register_field_group_page_section_group() {
 
